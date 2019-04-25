61e45900535a1bab1cc6dea076acc66878477f35
Alison Golden <alisong779@yahoo.com>
2019-04-25 17:14:25 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song 
+  attr_accessor :name, :artist 
+  
+  def initialize(name) 
+    @name = name 
+    @@songs = []
+    @@songs << name 
+  end 
+  
+  def artist_name 
+    self.artist.name 
+  end 
+  
+  def self.all 
+    @@songs 
+  end 
+  
+end 
